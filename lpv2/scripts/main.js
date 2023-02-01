b16d8387b0f5037c67dc669742d939f5696e61cf
dmytroS1~ <dmytro.sukhorukov@clicklogiq.com>
2023-02-01 16:54:36 +0200

    version 2 added

---
+++ lpv2/scripts/main.js
@@ -0,0 +1,26 @@
+$(".others__slider").slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 3.99,
+  slidesToScroll: 1,
+  initialSlide: 0,
+});
+
+$(".jackpot-sl__slider").slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 3.99,
+  slidesToScroll: 1,
+  initialSlide: 0,
+});
+
+$(".megaways-sl__slider").slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 3.99,
+  slidesToScroll: 1,
+  initialSlide: 0,
+});
